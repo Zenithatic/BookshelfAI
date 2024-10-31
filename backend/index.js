8e7f95e4b8afe996f5cda0bd1c4ca31ecd436722
Patrick <patmc55555@gmail.com>
2024-10-31 17:44:20 -0400

    pls

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,12 @@ const port = 3000
 const app = express()
 app.use(helmet())
 
-app.use(cors({
-    origin: ['https://bookshelfai.onrender.com'],
-    credentials: true,
-    methods: "GET,POST,PUT,DELETE"
-}))
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://bookshelfai.onrender.com");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
 
 app.listen(port, () => {
     console.log("BookshelfAI Backend is listening on port 3000.");
